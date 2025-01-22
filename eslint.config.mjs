98570704db9dd9c732de5309e34dd1cae36eab9b
Demiana 122 <abdelrahman.elgammal@gmail.com>
2025-01-22 10:35:04 +0200

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,20 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+// ];
+
+// export default eslintConfig;
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -13,4 +30,24 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
-export default eslintConfig;
+export default {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "no-unused-vars": "off", // تعطيل قاعدة المتغيرات غير المستخدمة
+    "@typescript-eslint/no-unused-vars": "off", // تعطيل نفس القاعدة لـ TypeScript
+    "react/react-in-jsx-scope": "off", // إلغاء الحاجة لاستيراد React في كل ملف JSX
+  },
+};
